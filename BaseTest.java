0b215bd410d04a1eb6892c99c98f63399c1e1b4e
Kirill Vershinin <verkirill@yandex.ru>
2019-11-14 22:50:35 +0400

    новый файл:    BaseTest.java 	новый файл:    secondTest.java

---
+++ BaseTest.java
@@ -0,0 +1,24 @@
+package ru.yandex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    public WebDriver driver;
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "/home/kir/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("http://yandex.ru");
+    }
+
+    @After
+    public void Close(){
+        driver.quit();
+    }
+
+}
